66465ebb1d0d9680642497fbbda056778c10aca1
DESKTOP-BE6TUIP\Xinyu <xd522@ic.ac.uk>
2023-01-30 17:10:54 +0000

    second submission

--- data.py
+++ data.py
@@ -21,19 +21,20 @@ df = pd.read_csv("Moon_WAC_Training/labels/lunar_crater_database_robbins_train.c
 df_s = df[["LAT_CIRC_IMG", "LON_CIRC_IMG", "DIAM_CIRC_IMG"]].dropna()
 data = np.asarray(df_s)
 
+# max_long,min_long,max_lat,min_lat
 extents = [[-90, -180, 0, -45], [-90, -180, 45, 0], [0, -90, 0, -45], [0, -90, 45, 0]]
 Names = ["A", "B", "C", "D"]
 shapes = []
 imgs = []
 for name in Names:
-    img = cv2.imread(os.path.join("Moon_WAC_Training/images","Lunar_" + name + ".jpg"))
+    img = cv2.imread(os.path.join("Moon_WAC_Training/images", "Lunar_" + name + ".jpg"))
     shapes.append(img.shape)
     imgs.append(img)
 
 ratios_w = []
 ratios_h = []
 for i, extent in enumerate(extents):
-    w, h = shapes[i][:-1]
+    h, w = shapes[i][:-1]
     w_ratio = abs(extent[0] - extent[1]) / w
     h_ratio = abs(extent[2] - extent[3]) / h
     ratios_w.append(w_ratio)
@@ -55,10 +56,10 @@ for lat, long, l in data:
     w_ratio = ratios_w[region]
     h_ratio = ratios_h[region]
     x = int(abs(long - extent[1]) / w_ratio)
-    y = int(abs(lat - extent[3]) / h_ratio)
+    y = int(abs(lat - extent[2]) / h_ratio)
     radius = int(5 * l)
     cv2.rectangle(img, (int(x - radius), int(y - radius)), (int(x + radius), int(y + radius)), (0, 255, 255), 2, 4)
 
 for i, img in enumerate(imgs):
     name = Names[i]
-    cv2.imwrite(os.path.join("moon", "Lunar_"+name+'.png'), img)
+    cv2.imwrite(os.path.join("moon", "Lunar_" + name + '.png'), img)
